4a895f68352e316b530e01ea970e763b25a10daf
KhalilGibrotha <Alexgambino22@gmail.com>
2019-07-08 14:46:06 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,14 @@
+import requests
 
+BASE_URL = "https://battlefieldtracker.com/bfv/profile/origin/
+BASE_API_URL = "https://api.tracker.gg/api/v1/bfv/"
+PROFILE_URL = BASE_API_URL + "profile/origin/"
+LAST_GAMES = BASE_API_URL + "gamereports/origin/latest/"
+GAME_REPORT = BASE_API_URL + "gamereports/origin/direct/"
+
+resp_profile = requests.get(PROFILE_URL)
+if resp_profile.status_code !=200:
+    #Something bad happened
+    raise ApiError('GET /tasks/ {}'.format(respon_profile.status_code))
+for todo_item in resp_profile.json():
+    print('{} {}'.format(todo_item{'id'}, todo_item['summary']))
